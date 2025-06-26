21b1579cb80f9b544588a6a055c911f48fa0d3c0
freirart <artur.santos@stone.com.br>
2025-06-26 12:06:44 -0300

    interceptors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { Public } from './common/decorators/public.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Public()
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    await new Promise((resolve) =>
+      setTimeout(
+        resolve,
+        +this.configService.getOrThrow<number>('API_TIMEOUT') + 500,
+      ),
+    );
     return this.appService.getHello();
   }
 }
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ApiKeyGuard } from './api-key/api-key.guard';
+import { TimeoutInterceptor } from './timeout/timeout.interceptor';
+import { WrapResponseInterceptor } from './wrap-response/wrap-response.interceptor';
 
 @Module({
   imports: [ConfigModule],
@@ -9,6 +11,14 @@ import { ApiKeyGuard } from './api-key/api-key.guard';
     {
       provide: APP_GUARD,
       useClass: ApiKeyGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: WrapResponseInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
     },
   ],
 })
