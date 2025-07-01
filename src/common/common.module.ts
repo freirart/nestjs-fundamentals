dc2c497569886dc745e802c4dbb442545c2582cb
freirart <artur.santos@stone.com.br>
2025-07-01 10:55:36 -0300

    typed config

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,12 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
+import guardsConfig, { validationSchema } from './guards/guards.config';
 import { TimeoutInterceptor } from './interceptors/timeout/timeout.interceptor';
 import { WrapResponseInterceptor } from './interceptors/wrap-response/wrap-response.interceptor';
 import { LoggingMiddleware } from './middlewares/logging/logging.middleware';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      validationSchema,
+      load: [guardsConfig],
+    }),
+  ],
   providers: [
     {
       provide: APP_GUARD,
