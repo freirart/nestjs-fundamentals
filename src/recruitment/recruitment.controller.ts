cc03002f647c0aa29dd555a2f9ca4a206f715def
freirart <artur.santos@stone.com.br>
2025-07-02 14:08:16 -0300

    swagger auth n insomnia request

--- src/recruitment/recruitment.controller.ts
+++ src/recruitment/recruitment.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { RecruitDto } from './dto/recruit.dto';
 import { RecruitmentService } from './recruitment.service';
 
+@ApiBearerAuth()
 @Controller('recruitment')
 export class RecruitmentController {
   constructor(private readonly recruitmentService: RecruitmentService) {}
